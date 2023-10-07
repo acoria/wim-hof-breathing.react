bbc90eb0d352672b6d3bdef3141bf44b28b05c76
acoria <tewes.verena@gmail.com>
2023-10-07 15:56:34 +0200

    Add color.css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
+import "./color.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
