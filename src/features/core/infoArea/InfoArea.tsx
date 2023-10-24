c6f005d4eb22bda6cf6aed52674cb79a71c24da1
acoria <tewes.verena@gmail.com>
2023-10-24 14:55:20 +0200

    Fix missing key error in InfoArea component

--- src/features/core/infoArea/InfoArea.tsx
+++ src/features/core/infoArea/InfoArea.tsx
@@ -1,24 +1,25 @@
-import { ReactNode } from "react";
+import { ReactNode, useMemo } from "react";
 import { IInfoAreaProps } from "./IInfoAreaProps";
 import styles from "./InfoArea.module.css";
 import { style } from "../../../utils/style";
+import React from "react";
 
 export const InfoArea: React.FC<IInfoAreaProps> = (props) => {
-  const buildSteps = (): ReactNode[] | undefined => {
+  const buildSteps = useMemo((): ReactNode | undefined => {
     return props.steps?.map((step, index) => {
       return (
-        <>
+        <React.Fragment key={index}>
           <div>{index + 1}.</div>
           <div>{step}</div>
-        </>
+        </React.Fragment>
       );
     });
-  };
+  }, [props.steps]);
 
   return (
     <div className={style(styles.infoArea, props.className)}>
       {props.description && <p>{props.description}</p>}
-      {props.steps && <div className={styles.steps}>{buildSteps()}</div>}
+      {props.steps && <div className={styles.steps}>{buildSteps}</div>}
       {props.additionalInfo && <p>{props.additionalInfo}</p>}
     </div>
   );
