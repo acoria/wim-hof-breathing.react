a34a3ea850c17d9a826e4db554f31a24b5f5b44e
acoria <tewes.verena@gmail.com>
2024-05-02 22:14:12 +0200

    Update android and keep screen on

--- android/app/src/main/java/io/acoria/wimHofBreathing/MainActivity.java
+++ android/app/src/main/java/io/acoria/wimHofBreathing/MainActivity.java
@@ -1,6 +1,16 @@
 package io.acoria.wimHofBreathing;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+    
 }
